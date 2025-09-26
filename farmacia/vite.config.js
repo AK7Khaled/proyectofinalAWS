ff4433ef06d3f494b5ba63121b37aaccf48c5c17
AK7Khaled <acmijail@gmail.com>
2025-09-26 18:12:15 -0500

    Update vite.config.js

--- farmacia/vite.config.js
+++ farmacia/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'farmaciadb.c1qy26iwyklf.us-east-2.rds.amazonaws.com:3306', // redirige /api a tu backend
+      '/api': 'farmaciadb.c1qy26iwyklf.us-east-2.rds.amazonaws.com:5000', // redirige /api a tu backend
     },
     allowedHosts: ['ec2-18-191-42-211.us-east-2.compute.amazonaws.com'],
     host: true,    
